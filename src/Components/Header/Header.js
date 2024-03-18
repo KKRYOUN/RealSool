348cd2ab0784d0ccedf30453788420bdb194e942
krkim <kkryoun300@gmail.com>
2024-03-18 19:26:06 +0900

    헤더 부분 수정 : grid 사용 및 이미지 추가

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import logo from '../../asset/REAL_SOOL_LOGO.png'
+import { AppBar, Grid, Toolbar, Typography, Button } from '@mui/material';
 
 const Header = () => {
   return (
-    <AppBar position="static" style={{ backgroundColor: '#9c27b0' }}> {/* 이 부분에서 배경색을 설정합니다. */}
+    <AppBar position="static" style={{ backgroundColor: '#a4134b' }}>
       <Toolbar>
-        {/* 로고가 위치할 자리, 이미지나 텍스트 로고를 넣을 수 있습니다. */}
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          우리술 소개
-        </Typography>
-        
-        {/* 네비게이션 아이템 */}
-        <Button color="inherit">우리술 소개</Button>
-        <Button color="inherit">양조장</Button>
-        <Button color="inherit">우리술 정보</Button>
-        <Button color="inherit">찐들의 모임</Button>
-        
-        {/* 로그인 버튼 */}
-        <Button color="inherit">로그인/회원가입</Button>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={4} lg={7}>
+            {/* xs: extra-small devices (모바일)
+                sm: small devices (태블릿)
+                md: medium devices (작은 랩탑)
+                lg: large devices (대형 랩탑, 데스크탑)
+                모바일에서는 전체 너비(12), 태블릿에서는 반 너비(6), 작은 랩탑에서는 1/3 너비(md), 대형 랩탑에서는 1/4 너비(lg)를 차지
+            */}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <img src={logo} alt="REAL_SOOL_LOGO" style={{ maxHeight: '50px' }} />
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4} lg={1}>
+            <Button color="inherit">우리술 소개</Button>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4} lg={1}>
+            <Button color="inherit">양조장</Button>
+          </Grid>
+          
+          <Grid item xs={12} sm={6} md={4} lg={1}>
+            <Button color="inherit">우리술 정보</Button>
+          </Grid>
+          
+          <Grid item xs={12} sm={6} md={4} lg={1}>
+            <Button color="inherit">찐들의 모임</Button>
+          </Grid>
+          
+          <Grid item xs={12} sm={6} md={4} lg={1}>
+            <Button color="inherit">로그인/회원가입</Button>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
