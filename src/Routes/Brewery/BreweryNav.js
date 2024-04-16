4f941cb66f000187d47adc91949c78f058533cca
kong8 <kong8249@naver.com>
2024-04-16 21:27:33 +0900

    사라: 양조장 상세 보기 페이지 구현 완료

--- src/Routes/Brewery/BreweryNav.js
+++ src/Routes/Brewery/BreweryNav.js
@@ -4,13 +4,11 @@ import { AppBar, Toolbar, Button } from '@mui/material';
 
 const BreweryNav = () => {
   return (
-    <AppBar position="static" style={{ margin: '-33px auto 0', backgroundColor: '#6a4843', maxWidth: '77%', alignItems: 'center'}}>
-    {/* <AppBar position="static" style={{ margin: '-33px auto 0', backgroundColor: 'transparent', maxWidth: '77%', alignItems: 'center'}}> */}
+    <AppBar position="static" style={{ margin: '-33px auto 0', backgroundColor: '#6a4843', maxWidth: '77%', alignItems: 'center' }}>
       <Toolbar>
-        
         {/* 네비게이션 아이템 */}
-        <Button color="inherit" component={Link} to="/brewery" style={{fontSize: '17px', marginRight: '55px'}}>양조장 지도</Button>
-        <Button color="inherit" component={Link} to="/brewery/breweryList" style={{fontSize: '17px', marginRight: '55px'}}>양조장 상세 소개</Button>
+        <Button color="inherit" component={Link} to="/brewery" style={{ fontSize: '17px' }}>양조장 지도</Button>
+        <Button color="inherit" component={Link} to="/brewery/breweryList" style={{ fontSize: '17px', marginLeft: '55px' }}>양조장 상세 소개</Button>
 
       </Toolbar>
     </AppBar>
--- src/Routes/Brewery/BreweryView.js
+++ src/Routes/Brewery/BreweryView.js
@@ -1,11 +1,10 @@
 import { React } from "react";
 import { useParams } from 'react-router-dom';
 
-import { Container, Typography, Grid, Card, CardContent, CardMedia, CardHeader, Box } from '@mui/material';
+import { Typography, Box, Table, TableCell, TableContainer, TableRow, Grid, Card, CardContent, CardActions, Button, CardMedia } from '@mui/material';
 import { styled } from '@mui/system';
 
 import menuImage from './Image/menuImage.png';
-import background from '../../asset/background.jpg';
 
 const BreweryView = ({ breweries }) => {
   const CustomDiv = styled('div')({
@@ -26,52 +25,123 @@ const BreweryView = ({ breweries }) => {
   const brewery = breweries.find(brewery => brewery.num === num);
 
   return (
-    <div style={{ backgroundImage: `url(${background})` }}>
+    <div>
       <CustomDiv>
         <Typography variant='h2' color='white' align='center'>
-          양조장 보기
+          양조장 상세 소개
         </Typography>
       </CustomDiv>
 
-      <Container maxWidth="lg">
-        <Grid container spacing={1} alignItems="center">
-          <Grid item xs={12} sm={12} md={12} lg={4}>
-            <Box
-              sx={{
-                backgroundImage: `url(${brewery.image})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                height: 300,
-                width: 300,
-                my: 4,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                p: 2,
-                border: '2px solid grey'
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={12} lg={8}>
-            <Card>
-              <CardHeader
-                title={brewery.name}
-              />
-              <Typography variant="body1">
-                주소 : {brewery.address}
-              </Typography>
-              <Typography variant="body1">
-                주종 : {brewery.category}
-              </Typography>
-              <Typography variant="body1">
-                연락처 : {brewery.tel}
-              </Typography>
+      <Box
+        height={50}
+        width='80%'
+        my={4}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap={4}
+        sx={{ borderTop: '3px solid', borderBottom: '1px solid' }}
+        margin='20px auto'
+        padding='16px 0'
+      >
+        <Typography variant='h5' align='center'>
+          {brewery.name}
+        </Typography>
+      </Box>
+
+      <Box
+        height={480}
+        width="80%"
+        my={4}
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+        sx={{
+          backgroundImage: `url(${brewery.image})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+        margin='30px auto'
+      />
+
+      <Box
+        width='80%'
+        margin='0 auto'>
+        <TableContainer sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+          <Table sx={{ borderTop: '2px solid' }} aria-label="simple table">
+            <TableRow>
+              <TableCell align="center" sx={{ backgroundColor: "#f2f0ed", borderBottom: "1px solid #bfbfbf", width: '16%' }}>
+                <Typography variant='body1' align='center' style={{ fontWeight: 'bold' }}>
+                  주&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;소
+                </Typography>
+              </TableCell>
+              <TableCell sx={{ borderBottom: "1px solid #bfbfbf" }}>{brewery.address}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="center" sx={{ backgroundColor: "#f2f0ed", borderBottom: "1px solid #bfbfbf" }}>
+                <Typography variant='body1' align='center' style={{ fontWeight: 'bold' }}>
+                  주&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;종
+                </Typography>
+              </TableCell>
+              <TableCell sx={{ borderBottom: "1px solid #bfbfbf" }}>{brewery.category}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="center" sx={{ backgroundColor: "#f2f0ed", borderBottom: "1px solid #bfbfbf" }}>
+                <Typography variant='body1' align='center' style={{ fontWeight: 'bold' }}>
+                  연&nbsp;락&nbsp;처
+                </Typography>
+              </TableCell>
+              <TableCell sx={{ borderBottom: "1px solid #bfbfbf" }}>{brewery.tel}</TableCell>
+            </TableRow>
+          </Table>
+        </TableContainer>
+      </Box>
+
+      <Grid item style={{ width: '80%', margin: '16px auto 0' }}>
+        <Typography variant='body1' align='center' p={2} style={{ fontWeight: 'bold', backgroundColor: '#313a46', color: '#fff' }}>
+          주요 제품
+        </Typography>
+      </Grid>
+      <Grid container spacing={2} style={{ marginTop: '18px', width: '80%', margin: '0 auto', paddingLeft: 0 }}>
+        {brewery.drinks.map((card, index) => (
+          <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
+            <Card variant="outlined">
+              <Box
+                height={300}
+                width="80%"
+                display='flex'
+                alignItems='center'
+                justifyContent='center'
+                sx={{
+                  backgroundImage: `url(${card.image})`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center',
+                }}
+                margin='0 auto'
+              >
+              </Box>
+              <CardContent style={{ height: '60px', textAlign: 'center', backgroundColor: '#f5f5f5' }}>
+                <Typography variant="h6" component="h2" style={{ marginBottom: '5px' }}>
+                  {card.name}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  {card.ml} | {card.percent}
+                </Typography>
+              </CardContent>
             </Card>
           </Grid>
-        </Grid>
-      </Container>
-
+        ))}
+      </Grid>
+      <Grid item style={{ width: '80%', margin: '30px auto 0', textAlign:'center' }}>
+        <Button variant="contained" style={{ backgroundColor: '#a4134b', width: '160px', height: '50px'}} href="/brewery/breweryList">
+          목록
+        </Button>
+      </Grid>
     </div>
   )
 }
